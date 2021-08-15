2f9274db4003d0fac770a5b13e0f8dfe32cc20d3
Nicklas Löf <nicklaslof76@gmail.com>
2021-08-15 02:16:02 +0200

    More work on minify and reduce zip-file size

--- compile.sh
+++ compile.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 rm dist.zip
-mkdir -p dist/lib
+mkdir -p dist
 cd src
-cp index.html i.css ../dist/
-cp lib/t.js ../dist/lib
-cp c.js ../dist/
 rollup i.js --format cjs --file ../dist/bundle.js
 cd ../dist
 terser bundle.js -o i.js --compress --mangle --mangle-props reserved=["g","img","flush","bkg","cls","col"] --timings --toplevel --module
 rm bundle.js
-cd ..
-cd dist
+echo "<style>" > index-template.html
+cat ../src/i.css >> index-template.html
+echo "</style>" >> index-template.html
+echo "<canvas id=\"g\"></canvas><canvas id=\"t\"></canvas>" >> index-template.html
+echo "<script>" >> index-template.html
+cat ../src/lib/t.js >> index-template.html
+echo "</script>" >> index-template.html
+echo "<script>" >> index-template.html
+cat ../src/c.js >> index-template.html
+echo "</script>" >> index-template.html
+echo "<script type=\"module\" charset=\"utf8\">" >> index-template.html
+cat i.js >> index-template.html
+echo "</script>" >> index-template.html
+
+cat index-template.html | tr -d '\n' > index.html
+
+rm i.js index-template.html
 zip -r ../dist.zip *
 cd ..
 echo "ect:"
--- src/c.js
+++ src/c.js
@@ -1,4 +1,1 @@
-const TEXTURE_SIZE = 512;
-const FOV = 70;
-const WIDTH = 1600;
-const HEIGHT = 1200;
+const TEXTURE_SIZE=512;const FOV=70;const WIDTH=1600;const HEIGHT=1200;
