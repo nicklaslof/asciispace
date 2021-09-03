43160e8f0fcaa118ba35174f38b9bdcb4175e966
Nicklas Löf <nicklaslof76@gmail.com>
2021-09-03 08:29:38 +0200

    Hearts are a bit wider now. Forgot to change that when I changed the graphic for it earlier

--- src/entity/heart.js
+++ src/entity/heart.js
@@ -3,7 +3,7 @@ import Entity from "./entity.js";
 // The heart used in the health bar
 class Heart extends Entity{
     constructor() {
-        super(0,0,270,36,20,24,0xff0000ff,20,24,"h");
+        super(0,0,270,36,20,24,0xff0000ff,24,24,"h");
     }
 }
 
